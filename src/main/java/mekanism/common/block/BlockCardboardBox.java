697464d93914a46ef552c77d7482dadbf3cb03ea
TechnoParadox <gustavo.caliari@live.com>
2022-07-26 12:16:09 +0200

    Cardboard Tweaks+ Fixes
    -Cardboard is now single use.
    -Fixes some missing langs and textures for experimental features

--- src/main/java/mekanism/common/block/BlockCardboardBox.java
+++ src/main/java/mekanism/common/block/BlockCardboardBox.java
@@ -106,14 +106,14 @@ public class BlockCardboardBox extends BlockContainer
 					data.block.onPostBlockPlaced(world, x, y, z, data.meta);
 				}
 
-				float motion = 0.7F;
-				double motionX = (world.rand.nextFloat() * motion) + (1.0F - motion) * 0.5D;
-				double motionY = (world.rand.nextFloat() * motion) + (1.0F - motion) * 0.5D;
-				double motionZ = (world.rand.nextFloat() * motion) + (1.0F - motion) * 0.5D;
+				// float motion = 0.7F;
+				// double motionX = (world.rand.nextFloat() * motion) + (1.0F - motion) * 0.5D;
+				// double motionY = (world.rand.nextFloat() * motion) + (1.0F - motion) * 0.5D;
+				// double motionZ = (world.rand.nextFloat() * motion) + (1.0F - motion) * 0.5D;
 
-				EntityItem entityItem = new EntityItem(world, x + motionX, y + motionY, z + motionZ, itemStack);
+				// EntityItem entityItem = new EntityItem(world, x + motionX, y + motionY, z + motionZ, itemStack);
 
-				world.spawnEntityInWorld(entityItem);
+				// world.spawnEntityInWorld(entityItem);
 			}
 		}
 
