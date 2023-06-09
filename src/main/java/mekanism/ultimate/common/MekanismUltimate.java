4e8c389b852ddf15507d64de3c434fc63bcf2fb3
maggi373 <40539743+maggi373@users.noreply.github.com>
2023-06-09 15:27:29 +0200

    add mekanism ultimate module
    groundwork for the ultimate module

---
+++ src/main/java/mekanism/ultimate/common/MekanismUltimate.java
@@ -0,0 +1,9 @@
+package mekanism.ultimate.common;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = MekanismUltimate.MODID, useMetadata = true)
+@Mod.EventBusSubscriber()
+public class MekanismUltimate {
+    public static final String MODID = "mekanismultimate";
+}
